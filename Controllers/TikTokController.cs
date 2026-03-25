60a843732a6f593f596e79b131691e2f0d6a6557
mohammedyousiff <mohammedd.yousif09@gmail.com>
2026-03-25 21:05:38 +0300

    Create TikTokController.cs

---
+++ Controllers/TikTokController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace SponsorSaaS.Api.Controllers 
+{
+    [Route("api/tiktok")]
+    [ApiController]
+    public class TikTokController : ControllerBase
+    {
+        [HttpGet("callback")]
+        public async Task<IActionResult> Callback(string code)
+        {
+            if (string.IsNullOrEmpty(code)) return BadRequest("کۆدی تیکتۆک نەگەڕایەوە");
+
+            var clientKey = "aw30bjij28hamzh6"; 
+            var clientSecret = "pGC0XSpiHT5xakcRZo1TlqYnECFjGm07"; 
+            var redirectUri = "https://sponsors-76gg.onrender.com/api/tiktok/callback"; 
+
+            using var httpClient = new HttpClient();
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("client_key", clientKey),
+                new KeyValuePair<string, string>("client_secret", clientSecret),
+                new KeyValuePair<string, string>("code", code),
+                new KeyValuePair<string, string>("grant_type", "authorization_code"),
+                new KeyValuePair<string, string>("redirect_uri", redirectUri)
+            });
+
+            var response = await httpClient.PostAsync("https://open.tiktokapis.com/v2/oauth/token/", data);
+            var result = await response.Content.ReadAsStringAsync();
+
+            var htmlResponse = "<h2 style='color:green; text-align:center;'>پیرۆزە حەمە گیان!</h2>" +
+                               "<p style='text-align:center;'>تکایە ئەم کۆدەی خوارەوە هەمووی کۆپی بکە و بۆمی بنێرە:</p>" +
+                               $"<textarea style='width:100%; height:300px; direction:ltr;'>{result}</textarea>";
+
+            return Content(htmlResponse, "text/html; charset=utf-8");
+        }
+    }
+}
