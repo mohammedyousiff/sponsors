74f7ef9d68ebb97e6503980365282c08587515a1
mohammedyousiff <mohammedd.yousif09@gmail.com>
2026-03-25 22:13:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,40 +1,26 @@
 using Microsoft.AspNetCore.Hosting;
-
 using Microsoft.Extensions.Hosting;
-
-
+using Microsoft.Extensions.DependencyInjection; // ئەمە پێویستە بۆ ناسینەوەی AddHttpClient
 
 namespace SponsorSaaS.Api
-
 {
-
     public class Program
-
     {
-
         public static void Main(string[] args)
-
         {
-
             CreateHostBuilder(args).Build().Run();
-
         }
 
-
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-
             Host.CreateDefaultBuilder(args)
-
                 .ConfigureWebHostDefaults(webBuilder =>
-
                 {
+                    // لێرەدا HttpClient زیاد دەکەین بۆ ئەوەی باکئێند بتوانێت بە بێ کێشە پەیوەندی بە سێرڤەرەکانی تیکتۆکەوە بکات
+                    webBuilder.ConfigureServices(services => {
+                        services.AddHttpClient();
+                    });
 
                     webBuilder.UseStartup<Startup>();
-
                 });
-
     }
-
 }
-
