30161afc5422d832f80f51f1693d5035abd9a1ff
mohammedyousiff <mohammedd.yousif09@gmail.com>
2026-03-25 20:33:15 +0300

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -7,50 +7,48 @@ using Supabase;
 
 namespace SponsorSaaS.Api
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
 
-            // هێنانی زانیارییەکانی سوبابەیس
-            var url = Configuration["Supabase:Url"];
-            var key = Configuration["Supabase:Key"];
-            var options = new SupabaseOptions { AutoConnectRealtime = true };
+            // هێنانی زانیارییەکانی سوبابەیس
+            var url = Configuration["Supabase:Url"];
+            var key = Configuration["Supabase:Key"];
+            var options = new SupabaseOptions { AutoConnectRealtime = true };
 
-            // ناساندنی سوبابەیس بە سیستەمەکە
-            services.AddSingleton(provider => new Supabase.Client(url, key, options));
+            // ناساندنی سوبابەیس بە سیستەمەکە
+            services.AddSingleton(provider => new Supabase.Client(url, key, options));
 
-            // کردنەوەی CORS بۆ ئەوەی جاڤاسکریپتەکەمان بتوانێت قسەی لەگەڵ بکات
-         services.AddCors(options => {
-    options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-});
-        }
+            // کردنەوەی CORS بۆ ئەوەی جاڤاسکریپتەکەمان بتوانێت قسەی لەگەڵ بکات
+            services.AddCors(options => {
+                options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+        }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-            app.UseRouting();
-            app.UseCors("AllowAll");
-            app.UseAuthorization();
+            app.UseRouting();
+            app.UseCors("AllowAll");
+            app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
-
-فەرموو ئەمە هەموو کۆدەکەیە هەمویم بە چاککراوی بۆ بنێرەوە
